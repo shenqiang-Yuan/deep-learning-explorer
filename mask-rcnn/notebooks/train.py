ec707f736110c1001d41a32af655d96a0da6fc0b
shenqiang-Yuan <37180747+shenqiang-Yuan@users.noreply.github.com>
2018-07-17 21:25:16 +0800

    Update train.py

--- mask-rcnn/notebooks/train.py
+++ mask-rcnn/notebooks/train.py
@@ -1,1 +1,123 @@
 import os
+import sys
+import random
+import math
+import re
+import time
+import numpy as np
+import cv2
+import matplotlib
+import matplotlib.pyplot as plt
+
+sys.path.insert(0, '../libraries')
+from mrcnn.config import Config
+import mrcnn.utils as utils
+import mrcnn.model as modellib
+import mrcnn.visualize as visualize
+from mrcnn.model import log
+import mcoco.coco as coco
+import mextra.utils as extra_utils
+
+%matplotlib inline
+%config IPCompleter.greedy=True
+
+HOME_DIR = 'deep-learning-explorer'
+DATA_DIR = os.path.join(HOME_DIR, "data/shapes")
+WEIGHTS_DIR = os.path.join(HOME_DIR, "data/weights")
+MODEL_DIR = os.path.join(DATA_DIR, "logs")
+
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(WEIGHTS_DIR, "mask_rcnn_coco.h5")
+# Download COCO trained weights from Releases if needed
+if not os.path.exists(COCO_MODEL_PATH):
+    utils.download_trained_weights(COCO_MODEL_PATH)
+
+def get_ax(rows=1, cols=1, size=8):
+    """Return a Matplotlib Axes array to be used in
+    all visualizations in the notebook. Provide a
+    central point to control graph sizes.
+    
+    Change the default size attribute to control the size
+    of rendered images
+    """
+    _, ax = plt.subplots(rows, cols, figsize=(size*cols, size*rows))
+    return ax
+  
+dataset_train = coco.CocoDataset()
+dataset_train.load_coco(DATA_DIR, subset="shapes_train", year="2018")
+dataset_train.prepare()
+
+dataset_validate = coco.CocoDataset()
+dataset_validate.load_coco(DATA_DIR, subset="shapes_validate", year="2018")
+dataset_validate.prepare()
+
+dataset_test = coco.CocoDataset()
+dataset_test.load_coco(DATA_DIR, subset="shapes_test", year="2018")
+dataset_test.prepare()
+
+# Load and display random samples
+image_ids = np.random.choice(dataset_train.image_ids, 4)
+for image_id in image_ids:
+    image = dataset_train.load_image(image_id)
+    mask, class_ids = dataset_train.load_mask(image_id)
+    visualize.display_top_masks(image, mask, class_ids, dataset_train.class_names)
+
+image_size = 64
+rpn_anchor_template = (1, 2, 4, 8, 16) # anchor sizes in pixels
+rpn_anchor_scales = tuple(i * (image_size // 16) for i in rpn_anchor_template)
+
+class ShapesConfig(Config):
+    """Configuration for training on the shapes dataset.
+    """
+    NAME = "shapes"
+
+    # Train on 1 GPU and 2 images per GPU. Put multiple images on each
+    # GPU if the images are small. Batch size is 2 (GPUs * images/GPU).
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 3  # background + 3 shapes (triangles, circles, and squares)
+
+    # Use smaller images for faster training. 
+    IMAGE_MAX_DIM = image_size
+    IMAGE_MIN_DIM = image_size
+    
+    # Use smaller anchors because our image and objects are small
+    RPN_ANCHOR_SCALES = rpn_anchor_scales
+
+    # Aim to allow ROI sampling to pick 33% positive ROIs.
+    TRAIN_ROIS_PER_IMAGE = 32
+
+    STEPS_PER_EPOCH = 400
+
+    VALIDATION_STEPS = STEPS_PER_EPOCH / 20
+    
+config = ShapesConfig()
+#config.display()
+
+model = modellib.MaskRCNN(mode="training", config=config, model_dir=MODEL_DIR)
+inititalize_weights_with = "coco"  # imagenet, coco, or last
+
+if inititalize_weights_with == "imagenet":
+    model.load_weights(model.get_imagenet_weights(), by_name=True)    
+elif inititalize_weights_with == "coco":
+    model.load_weights(COCO_MODEL_PATH, by_name=True,
+                       exclude=["mrcnn_class_logits", "mrcnn_bbox_fc", 
+                                "mrcnn_bbox", "mrcnn_mask"])  
+elif inititalize_weights_with == "last":
+    # Load the last model you trained and continue training
+    model.load_weights(model.find_last()[1], by_name=True)
+    
+
+model.train((dataset_traindataset , dataset_validate, 
+            learning_rate=config.LEARNING_RATE, 
+            epochs=2,
+            layers='heads')
+#fine tune
+'''
+model.train((dataset_traindataset , dataset_validate, 
+            learning_rate=config.LEARNING_RATE / 10,
+            epochs=3, # starts from the previous epoch, so only 1 additional is trained 
+            layers="all")
+'''
