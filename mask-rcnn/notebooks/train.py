69a7d6e8bdfbf0742d142d9fc7401fb190324a5d
shenqiang-Yuan <37180747+shenqiang-Yuan@users.noreply.github.com>
2018-07-18 16:20:38 +0800

    Update train.py

--- mask-rcnn/notebooks/train.py
+++ mask-rcnn/notebooks/train.py
@@ -6,6 +6,7 @@ import re
 import time
 import numpy as np
 import cv2
+import tensorflow as tf
 import matplotlib
 import matplotlib.pyplot as plt
 
@@ -18,16 +19,39 @@ from mrcnn.model import log
 import mcoco.coco as coco
 import mextra.utils as extra_utils
 
-#%matplotlib inline
-#%config IPCompleter.greedy=True
 
-HOME_DIR = 'master'
-DATA_DIR = os.path.join(HOME_DIR, "data/shapes")
-WEIGHTS_DIR = os.path.join(HOME_DIR, "data/weights")
-MODEL_DIR = os.path.join(DATA_DIR, "logs")
+tf.app.flags.DEFINE_string(
+    'HOME_DIR',
+    'master',
+    'the home directory,default value is "master"')
+tf.app.flags.DEFINE_string(
+    'DATA_DIR',
+    'master/data/shapes',
+    'the data directory,default value is "master/data/shapes"')
+tf.app.flags.DEFINE_string(
+    'WEIGHTS_DIR',
+    'master/data/weights',
+    'the data directory,default value is "master/data/weights"')
+tf.app.flags.DEFINE_string(
+    'MODEL_DIR',
+    'master/data/shapes/logs',
+    'the data directory,default value is "master/data/shapes/logs"')
 
-# Local path to trained weights file
-COCO_MODEL_PATH = os.path.join(WEIGHTS_DIR, "mask_rcnn_coco.h5")
+tf.app.flags.DEFINE_string(
+    'COCO_MODEL_PATH',
+    'master/data/weights/mask_rcnn_coco.h5',
+    'the data directory,default value is "master/data/weights/mask_rcnn_coco.h5"')
+tf.app.flags.DEFINE_string(
+    'inititalize_weights_with',
+    'coco',
+    'which dataset is used to acquire the initialize wights,default value is "coco"')
+# HOME_DIR = 'master'
+# DATA_DIR = os.path.join(HOME_DIR, "data/shapes")
+# WEIGHTS_DIR = os.path.join(HOME_DIR, "data/weights")
+# MODEL_DIR = os.path.join(DATA_DIR, "logs")
+
+# # Local path to trained weights file
+# COCO_MODEL_PATH = os.path.join(WEIGHTS_DIR, "mask_rcnn_coco.h5")
 # Download COCO trained weights from Releases if needed
 if not os.path.exists(COCO_MODEL_PATH):
     utils.download_trained_weights(COCO_MODEL_PATH)
@@ -43,28 +67,28 @@ def get_ax(rows=1, cols=1, size=8):
     _, ax = plt.subplots(rows, cols, figsize=(size*cols, size*rows))
     return ax
   
-dataset_train = coco.CocoDataset()
-dataset_train.load_coco(DATA_DIR, subset="shapes_train", year="2018")
-dataset_train.prepare()
+# dataset_train = coco.CocoDataset()
+# dataset_train.load_coco(DATA_DIR, subset="shapes_train", year="2018")
+# dataset_train.prepare()
 
-dataset_validate = coco.CocoDataset()
-dataset_validate.load_coco(DATA_DIR, subset="shapes_validate", year="2018")
-dataset_validate.prepare()
+# dataset_validate = coco.CocoDataset()
+# dataset_validate.load_coco(DATA_DIR, subset="shapes_validate", year="2018")
+# dataset_validate.prepare()
 
-dataset_test = coco.CocoDataset()
-dataset_test.load_coco(DATA_DIR, subset="shapes_test", year="2018")
-dataset_test.prepare()
+# dataset_test = coco.CocoDataset()
+# dataset_test.load_coco(DATA_DIR, subset="shapes_test", year="2018")
+# dataset_test.prepare()
 
-# Load and display random samples
-image_ids = np.random.choice(dataset_train.image_ids, 4)
-for image_id in image_ids:
-    image = dataset_train.load_image(image_id)
-    mask, class_ids = dataset_train.load_mask(image_id)
-    visualize.display_top_masks(image, mask, class_ids, dataset_train.class_names)
+# # Load and display random samples
+# image_ids = np.random.choice(dataset_train.image_ids, 4)
+# for image_id in image_ids:
+#     image = dataset_train.load_image(image_id)
+#     mask, class_ids = dataset_train.load_mask(image_id)
+#     visualize.display_top_masks(image, mask, class_ids, dataset_train.class_names)
 
-image_size = 64
-rpn_anchor_template = (1, 2, 4, 8, 16) # anchor sizes in pixels
-rpn_anchor_scales = tuple(i * (image_size // 16) for i in rpn_anchor_template)
+# image_size = 64
+# rpn_anchor_template = (1, 2, 4, 8, 16) # anchor sizes in pixels
+# rpn_anchor_scales = tuple(i * (image_size // 16) for i in rpn_anchor_template)
 
 class ShapesConfig(Config):
     """Configuration for training on the shapes dataset.
@@ -93,27 +117,27 @@ class ShapesConfig(Config):
 
     VALIDATION_STEPS = STEPS_PER_EPOCH / 20
     
-config = ShapesConfig()
+# config = ShapesConfig()
 #config.display()
 
-model = modellib.MaskRCNN(mode="training", config=config, model_dir=MODEL_DIR)
-inititalize_weights_with = "coco"  # imagenet, coco, or last
+# model = modellib.MaskRCNN(mode="training", config=config, model_dir=MODEL_DIR)
+# inititalize_weights_with = "coco"  # imagenet, coco, or last
 
-if inititalize_weights_with == "imagenet":
-    model.load_weights(model.get_imagenet_weights(), by_name=True)    
-elif inititalize_weights_with == "coco":
-    model.load_weights(COCO_MODEL_PATH, by_name=True,
-                       exclude=["mrcnn_class_logits", "mrcnn_bbox_fc", 
-                                "mrcnn_bbox", "mrcnn_mask"])  
-elif inititalize_weights_with == "last":
-    # Load the last model you trained and continue training
-    model.load_weights(model.find_last()[1], by_name=True)
+# if inititalize_weights_with == "imagenet":
+#     model.load_weights(model.get_imagenet_weights(), by_name=True)    
+# elif inititalize_weights_with == "coco":
+#     model.load_weights(COCO_MODEL_PATH, by_name=True,
+#                        exclude=["mrcnn_class_logits", "mrcnn_bbox_fc", 
+#                                 "mrcnn_bbox", "mrcnn_mask"])  
+# elif inititalize_weights_with == "last":
+#     # Load the last model you trained and continue training
+#     model.load_weights(model.find_last()[1], by_name=True)
     
-print("start training!")
-model.train((dataset_traindataset , dataset_validate, 
-            learning_rate=config.LEARNING_RATE, 
-            epochs=2,
-            layers='heads')
+# print("start training!")
+# model.train((dataset_traindataset , dataset_validate, 
+#             learning_rate=config.LEARNING_RATE, 
+#             epochs=2,
+#             layers='heads')
 #fine tune
 '''
 model.train((dataset_traindataset , dataset_validate, 
@@ -121,3 +145,53 @@ model.train((dataset_traindataset , dataset_validate,
             epochs=3, # starts from the previous epoch, so only 1 additional is trained 
             layers="all")
 '''
+FLAGS = tf.app.flags.FLAGS
+def main(_):
+    if not FLAGS.dataset_dir:
+        raise ValueError('You must supply the dataset directory with --dataset_dir')
+    if not os.path.exists(FLAGS.COCO_MODEL_PATH):
+        utils.download_trained_weights(FLAGS.COCO_MODEL_PATH)
+    config = ShapesConfig(FLAGS)
+    
+    dataset_train = coco.CocoDataset()
+    dataset_train.load_coco(DATA_DIR, subset="shapes_train", year="2018")
+    dataset_train.prepare()
+
+    dataset_validate = coco.CocoDataset()
+    dataset_validate.load_coco(DATA_DIR, subset="shapes_validate", year="2018")
+    dataset_validate.prepare()
+
+    dataset_test = coco.CocoDataset()
+    dataset_test.load_coco(DATA_DIR, subset="shapes_test", year="2018")
+    dataset_test.prepare()
+    
+    image_ids = np.random.choice(dataset_train.image_ids, 4)
+    for image_id in image_ids:
+        image = dataset_train.load_image(image_id)
+        mask, class_ids = dataset_train.load_mask(image_id)
+        visualize.display_top_masks(image, mask, class_ids, dataset_train.class_names)
+
+    image_size = 64
+    rpn_anchor_template = (1, 2, 4, 8, 16) # anchor sizes in pixels
+    rpn_anchor_scales = tuple(i * (image_size // 16) for i in rpn_anchor_template)
+    model = modellib.MaskRCNN(mode="training", config=config, model_dir=MODEL_DIR)
+    
+    inititalize_weights_with = FLAGS.inititalize_weights_with  # imagenet, coco, or last
+
+    if inititalize_weights_with == "imagenet":
+        model.load_weights(model.get_imagenet_weights(), by_name=True)    
+    elif inititalize_weights_with == "coco":
+        model.load_weights(COCO_MODEL_PATH, by_name=True,
+                       exclude=["mrcnn_class_logits", "mrcnn_bbox_fc", 
+                                "mrcnn_bbox", "mrcnn_mask"])  
+    elif inititalize_weights_with == "last":
+    # Load the last model you trained and continue training
+        model.load_weights(model.find_last()[1], by_name=True)
+    model.train((dataset_traindataset , dataset_validate, 
+            learning_rate=config.LEARNING_RATE, 
+            epochs=2,
+            layers='heads')
+                
+
+if __name__=='__main__':
+    tf.app.run()
