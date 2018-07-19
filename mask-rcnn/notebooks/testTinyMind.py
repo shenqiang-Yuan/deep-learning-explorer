cbe701ecd586860ff3444f4ad5aea25e209260ea
shenqiang-Yuan <37180747+shenqiang-Yuan@users.noreply.github.com>
2018-07-19 12:55:10 +0800

    Update testTinyMind.py

--- mask-rcnn/notebooks/testTinyMind.py
+++ mask-rcnn/notebooks/testTinyMind.py
@@ -2,10 +2,10 @@ import os
 import sys
 from tinyenv.flags import flags
 _FLAGS = flags()
-os.chdir("master")
+#current path:   /tinysrc/mask-rcnn/notebooks/testTinyMind.py
 def save_to_file(file_name, contents):
     fh = open(file_name, 'w+')
     fh.write(contents)
     fh.close()
 
-save_to_file('mobiles.txt', os.getcwd())
+save_to_file(_FLAGS.output_dir+'mobiles.txt', os.getcwd())
