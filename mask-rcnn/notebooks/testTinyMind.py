56048e9f3d72612fef320873e5fa3c9d7ffa0f34
shenqiang-Yuan <37180747+shenqiang-Yuan@users.noreply.github.com>
2018-07-19 10:49:29 +0800

    Create testTinyMind.py

---
+++ mask-rcnn/notebooks/testTinyMind.py
@@ -0,0 +1,10 @@
+import os
+import sys
+from tinyenv.flags import flags
+_FLAGS = flags()
+def save_to_file(file_name, contents):
+    fh = open(file_name, 'w')
+    fh.write(contents)
+    fh.close()
+
+save_to_file(_FLAGS.output_dir+'/mobiles.txt', os.getcwd())
